a5353612df0f170bcfdd33284d2cfc06f7bd023d
Juan Carlos <juancarloshernandezpuebla@ymail.com>
2024-12-08 18:59:16 -0600

    Add order by Last Name Asc method

--- src/main/java/com/rest_api/employee/dao/EmployeeRepository.java
+++ src/main/java/com/rest_api/employee/dao/EmployeeRepository.java
@@ -3,5 +3,9 @@ package com.rest_api.employee.dao;
 import com.rest_api.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
